697bd02acf47cefaee2975bd2d843314cb785978
bingbing-huang <bing2013@cs.washington.edu>
2017-07-19 11:14:47 -0700

    svm_author_id finished

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,14 +14,24 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from tools.email_preprocess import preprocess
-
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
 
 # features_train and features_test are the features for the training
 # and testing datasets, respectively
 # labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+# fit
+clf = GaussianNB()
+start = time()
+clf.fit(features_train, labels_train)
+print("training time: ", round(time() - start, 3), "s")
+# predict
+start = time()
+pred = clf.predict(features_test)
+print("predicting time: ", round(time() - start, 3), "s")
 
-# your code goes here
+# print(accuracy_score(pred, labels_test))
 
 
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -10,21 +10,30 @@
     
 import sys
 from time import time
+from tools.email_preprocess import preprocess
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+import numpy as np
 sys.path.append("../tools/")
-from email_preprocess import preprocess
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+clf = SVC(kernel="rbf", C=10000.)
+start = time()
+# features_train = features_train[:len(features_train) // 100]
+# labels_train = labels_train[:len(labels_train) // 100]
+clf.fit(features_train, labels_train)
+print("Time taken for training the model: ", (time() - start, "3"), "s\n")
+
+start = time()
+pred = clf.predict(features_test)
+print("Time taken for predicting: ", (time() - start, "3"), "s\n")
+
+# print("accuracy: ", accuracy_score(pred, labels_test))
+print(np.count_nonzero(pred))
 
 
-
-
-#########################################################
-### your code goes here ###
-
-#########################################################
 
 
