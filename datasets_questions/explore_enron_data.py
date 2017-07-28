b9a15ff86e84917f91969e47af15e8c2d43b7198
bingbing-huang <bing2013@cs.washington.edu>
2017-07-28 11:05:51 -0700

    datasets_question(non optional) finished

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -17,6 +17,24 @@
 
 import pickle
 
-enron_data = pickle.load((open("../final_project/final_project_dataset.pkl", "rb")),fix_imports=True)
+enron_data = pickle.load((open("../final_project/final_project_dataset.pkl", "rb")), fix_imports=True)
+
+# count = 0
+# for v in enron_data.values():
+#     if v["poi"]:
+#         count += 1
+# print(count)
+# Jeffrey K Skilling
+
+count_salary = 0
+count_email = 0
+for p in enron_data:
+    if enron_data[p]["salary"] != "NaN":
+        count_salary += 1
+    if enron_data[p]["email_address"] != "NaN":
+        count_email += 1
+
+print("count_salary: ", count_salary)
+print("count_email: ", count_email)
 
 
